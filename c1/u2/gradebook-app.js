a910542c5213b15bc8f040aef45420f1434070f2
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-06-19 15:09:27 +0700

    Convert the score to letter

--- c1/u2/gradebook-app.js
+++ c1/u2/gradebook-app.js
@@ -15,3 +15,28 @@ function getAverage(scores) {
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+// S2: Convert the student score to a letter grade
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/review-js-fundamentals-by-building-a-gradebook-app/step-2
+// • conditional statements (if, else if, and else)
+// • comparison operators (>, <, >=, <=, ===)
+
+function getGrade(score) {
+  if (score > 99) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
