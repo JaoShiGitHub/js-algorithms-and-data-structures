c3e2b92f4ffdff3ba39dbf14f9e50bf5588d2150
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-06-20 17:47:59 +0700

    Initialize buttons

--- c1/u3-role-playing-game/script.js
+++ c1/u3-role-playing-game/script.js
@@ -6,6 +6,41 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
+// buttons
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+//texts
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+//  initialize buttons
+
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+
+  text.innerText = "You enter the store.";
+
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+}
+
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
