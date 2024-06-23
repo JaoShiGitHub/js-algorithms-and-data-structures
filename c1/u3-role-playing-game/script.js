195a7554494d00fe845152c21c03357698445eeb
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-06-23 17:43:42 +0700

    Create 5th elem

--- c1/u3-role-playing-game/script.js
+++ c1/u3-role-playing-game/script.js
@@ -68,6 +68,17 @@ const locations = [
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
   },
+  {
+    name: "kill monster",
+    "button text": [
+      "Go to town square",
+      "Go to town square",
+      "Go to town square",
+    ],
+    "button functions": [goTown, goTown, goTown],
+    text: (text.innerText =
+      "The monster screams Arg! as it dies. You gain experience points and find gold."),
+  },
 ];
 
 //  initialize buttons
