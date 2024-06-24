507eca14d74fba3d2930de14f5903a0bd5e5674b
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-06-25 03:03:34 +0700

    create features

--- c1/u3-role-playing-game/script.js
+++ c1/u3-role-playing-game/script.js
@@ -78,6 +78,18 @@ const locations = [
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
   },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;",
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+  },
 ];
 
 //  initialize buttons
@@ -87,13 +99,14 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
 
 function goTown() {
@@ -178,7 +191,8 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
+  // health -= monsters[fighting].level;
+  health -= getMonsterAttackValue(monsters[fighting].level);
   monsterHealth -=
     weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
@@ -188,7 +202,15 @@ function attack() {
   } else if (monsterHealth <= 0) {
     defeatMonster();
   }
+
+  if (fighting === 2) {
+    winGame();
+  } else {
+    defeatMonster();
+  }
 }
+
+function getMonsterAttackValue(level) {}
 
 function dodge() {
   text.innerText = `You dodge the attack from the ${monsters[fighting].name}.`;
@@ -202,4 +224,24 @@ function defeatMonster() {
   update(locations[4]);
 }
 
-function lose() {}
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeaponIndex = 0;
+  inventory = ["stick"];
+
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+
+  goTown();
+}
