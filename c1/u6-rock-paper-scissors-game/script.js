19f1a9ac204b3b32515caff597ee53281e527398
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-06-28 16:39:44 +0700

    Create get result func

--- c1/u6-rock-paper-scissors-game/script.js
+++ c1/u6-rock-paper-scissors-game/script.js
@@ -1,11 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+
 // Random computer's choice
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
-  return `${options[Math.floor(Math.random(options) * options.length)]}`
+  // return `${options[Math.floor(Math.random(options) * options.length)]}`
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
-
-let playerScore = 0;
-let computerScore = 0;
 
 // Check if the player has won the round
 function hasPlayerWonTheRound(player, computer) {
@@ -15,3 +17,19 @@ function hasPlayerWonTheRound(player, computer) {
     (player === "Paper" && computer === "Rock")
   );
 }
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+
+
