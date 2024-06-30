27561da6b8fda5ccd871b54dfa4b8e44712d197d
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-01 06:15:04 +0700

    Update script.js

--- c1/u7-music-player/script.js
+++ c1/u7-music-player/script.js
@@ -1,9 +1,9 @@
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
-const nextButton = document.getElementById('next');
-const previousButton = document.getElementById('previous');
-const shuffleButton = document.getElementById('shuffle');
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
 
 const allSongs = [
   {
@@ -86,21 +86,21 @@ let userData = {
 };
 
 const playSong = (id) => {
-    const song = userData?.songs.find(song => song.id === id);
-    audio.src = song.src;
-    audio.title = song.title;
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
 
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
-  audio.currentTime = 0
+    audio.currentTime = 0;
   } else {
     audio.currentTime = userData?.songCurrentTime;
   }
   userData.currentSong = song;
   playButton.classList.add("playing");
 
-  highlightCurrentSong()
-  setPlayerDisplay()
-  setPlayButtonAccessibleText()
+  highlightCurrentSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -116,18 +116,20 @@ const playNextSong = () => {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
-    const nextSong = userData?.songs[currentSongIndex + 1]
-    playSong(nextSong.id)
-}
+    const nextSong = userData?.songs[currentSongIndex + 1];
 
-const playPreviousSong = () => {
-  if (userData?.currentSong === null) return;
-  else {
-    const currentSongIndex = getCurrentSongIndex();
-    const previousSong = userData?.songs[currentSongIndex - 1];
-    playSong(previousSong.id)
+    playSong(nextSong.id);
   }
 };
+
+const playPreviousSong = () =>{
+   if (userData?.currentSong === null) return;
+   else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+   }
 };
 
 // plays songs in a random order
@@ -135,37 +137,32 @@ const shuffle = () => {
   userData?.songs.sort(() => Math.random() - 0.5);
   userData.currentSong = null;
   userData.songCurrentTime = 0;
-// You should not use optional chaining for this step because you are explicitly setting the currentSong and songCurrentTime properties to be null and 0 respectively
+
   renderSongs(userData?.songs);
   pauseSong();
-  setPlayerDisplay()
-  setPlayButtonAccessibleText()
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
 };
 
 // Delete song feature
 const deleteSong = (id) => {
-    if (userData?.currentSong?.id === id) {
-    userData.currentSong  = null;
+  if (userData?.currentSong?.id === id) {
+    userData.currentSong = null;
     userData.songCurrentTime = 0;
-    pauseSong()
-    setPlayerDisplay()
+
+    pauseSong();
+    setPlayerDisplay();
   }
-  
-  userData.songs = userData?.songs.filter(song => song.id !== id);
-  renderSongs(userData?.songs);
-  highlightCurrentSong();
-  setPlayButtonAccessibleText();
 
-  if (userData?.songs) {
-    userData = {
-      songs: [...allSongs],
-      currentSong: null,
-      songCurrentTime: 0,
-  };
+  userData.songs = userData?.songs.filter((song) => song.id !== id);
+  renderSongs(userData?.songs); 
+  highlightCurrentSong(); 
+  setPlayButtonAccessibleText(); 
 
- if (userData?.songs.length === 0) {
+  if (userData?.songs.length === 0) {
     const resetButton = document.createElement("button");
     const resetText = document.createTextNode("Reset Playlist");
+
     resetButton.id = "reset";
     resetButton.ariaLabel = "Reset playlist";
     resetButton.appendChild(resetText);
@@ -173,14 +170,15 @@ const deleteSong = (id) => {
 
     resetButton.addEventListener("click", () => {
       userData.songs = [...allSongs];
-      renderSongs(sortSongs())
-      setPlayButtonAccessibleText()
-      resetButton.remove()
-    });
-  }
- }
-};
 
+      renderSongs(sortSongs()); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+
+  }
+
+};
 
 // Display the current song title and artist in the player display
 const setPlayerDisplay = () => {
@@ -192,103 +190,103 @@ const setPlayerDisplay = () => {
   playingSong.textContent = currentTitle ? currentTitle : "";
   songArtist.textContent = currentArtist ? currentArtist : "";
 };
-  
+
 // Highlight the current song
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
+
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
-  if (songToHighlight) {
-  songToHighlight.setAttribute("aria-current", "true")
-}
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {
-const songsHTML = array.map((song) => {
-  return `
-  <li id="song-${song.id}" class="playlist-song">
-  <button class="playlist-song-info" onclick="playSong(${song.id})">
-  <span class="playlist-song-title">${song.title}</span>
-  <span class="playlist-song-artist">${song.artist}</span>
-  <span class="playlist-song-duration">${song.duration}</span>
-  </button>
-  <button class="playlist-song-delete" aria-label="Delete ${song.title}" onclick="deleteSong(${song.id})"><svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg></button>
-  </li>`
-}).join("");
-playlistSongs.innerHTML = songsHTML;
+  const songsHTML = array
+    .map((song)=> {
+      return `
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
+      </li>
+      `;
+    })
+    .join("");
+
+  playlistSongs.innerHTML = songsHTML;
 };
 
 const setPlayButtonAccessibleText = () => {
   const song = userData?.currentSong || userData?.songs[0];
-  playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play");
+
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
 };
 
-const getCurrentSongIndex = () => {
-  return userData?.songs.indexOf(userData?.currentSong);
-};
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
-  if(!userData?.currentSong) {
+    if (userData?.currentSong === null) {
     // The first song in the playlist is played first
-  playSong(userData?.songs[0].id)
-} else {
+    playSong(userData?.songs[0].id);
+  } else {
     //  The currently playing song will continue to play when the play button is clicked
-    playSong(userData?.currentSong.id)
+    playSong(userData?.currentSong.id);
   }
-})
+});
 
-pauseButton.addEventListener("click", pauseSong);
+pauseButton.addEventListener("click",  pauseSong);
+
 nextButton.addEventListener("click", playNextSong);
+
 previousButton.addEventListener("click", playPreviousSong);
-shuffleButton.addEventListener("click", shuffle)
+
+shuffleButton.addEventListener("click", shuffle);
 
 audio.addEventListener("ended", () => {
   const currentSongIndex = getCurrentSongIndex();
   const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
-  if (nextSongExists) {
-    playNextSong();
-  } else {
-    userData.currentSong = null;
-    userData.songCurrentTime = 0;
-  }
+
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+    }
 pauseSong()
 setPlayerDisplay()
 highlightCurrentSong()
 setPlayButtonAccessibleText()
-})
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
-     if (a.title < b.title) {
+    if (a.title < b.title) {
       return -1;
     }
+
     if (a.title > b.title) {
       return 1;
     }
+
     return 0;
   });
+
   return userData?.songs;
-}
+};
 
 renderSongs(sortSongs());
-
-// Learnt Arrow function
-
-// const printGreeting = () => {
-//   console.log("Hello there!");
-// }
-// printGreeting()
-
-// const printMessage = org => {
-//   console.log(`${org} is awesome!`)
-// }
-
-// printMessage("freeCodeCamp");
-// • Implicit return
-// const addTwoNumbers = (num1, num2) =>  num1 + num2
-
-// console.log(addTwoNumbers(3, 4))
+setPlayButtonAccessibleText();
