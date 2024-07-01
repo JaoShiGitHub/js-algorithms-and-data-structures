819be9c12903f63da7d11e60443ccda94672e2e2
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-02 04:02:10 +0700

    switch / reverse / split

--- c1/u8-date-formatter/script.js
+++ c1/u8-date-formatter/script.js
@@ -6,7 +6,16 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
-
 const minutes = date.getMinutes();
+
 const formattedDate = `${day} - ${month} - ${year}`;
 currentDateParagraph.textContent = formattedDate;
+
+const exampleSentence = "selur pmaCedoCeerf".split("").reverse();
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+  }
+});
