1955ad6b6b533364b69871a8c11532a7d8bf2798
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-04 05:51:44 +0700

    add toggle & showModal

--- c2/u2-todo-app/script.js
+++ c2/u2-todo-app/script.js
@@ -9,3 +9,16 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+// store all the tasks along with their associated data, including title, due date, and description
+const taskData = [];
+//  used to track the state when editing and discarding tasks
+let currentTask = {}
+
+openTaskFormBtn.addEventListener("click", () => {
+  taskForm.classList.toggle("hidden");
+})
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal()
+})
