0ded67e94c3e72537ceb2389547e8d156513c37f
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-06 03:44:24 +0700

    add localStorage

--- c2/u2-todo-app/script.js
+++ c2/u2-todo-app/script.js
@@ -11,47 +11,48 @@ const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
 // store all the tasks along with their associated data, including title, due date, and description
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 //  used to track the state when editing and discarding tasks
-let currentTask = {}
+let currentTask = {};
 
 const addOrUpdateTask = () => {
-   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
-   const taskObj = {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     title: titleInput.value,
     date: dateInput.value,
     description: descriptionInput.value,
   };
 
-   if (dataArrIndex === -1) {
+  if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   } else {
     taskData[dataArrIndex] = taskObj;
   }
-   
-  updateTaskContainer();
-  reset();
-}
+
+  localStorage.setItem("data", JSON.stringify(taskData));
+  updateTaskContainer()
+  reset()
+};
 
 const updateTaskContainer = () => {
   tasksContainer.innerHTML = "";
-  
-    taskData.forEach(
+
+  taskData.forEach(
     ({ id, title, date, description }) => {
         (tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn" onclick="editTask(this)">Edit</button>
-          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `)
     }
   );
-}
+};
+
 
 const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
@@ -60,10 +61,12 @@ const deleteTask = (buttonEl) => {
 
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
+
+  localStorage.setItem("data", JSON.stringify(taskData));
 }
 
 const editTask = (buttonEl) => {
-   const dataArrIndex = taskData.findIndex(
+    const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
 
@@ -72,45 +75,48 @@ const editTask = (buttonEl) => {
   titleInput.value = currentTask.title;
   dateInput.value = currentTask.date;
   descriptionInput.value = currentTask.description;
-  
+
   addOrUpdateTaskBtn.innerText = "Update Task";
-   
-  taskForm.classList.toggle("hidden");
-};
+
+  taskForm.classList.toggle("hidden");  
+}
 
 const reset = () => {
+  addOrUpdateTaskBtn.innerText = "Add Task";
   titleInput.value = "";
   dateInput.value = "";
   descriptionInput.value = "";
-
   taskForm.classList.toggle("hidden");
-  currentTask = {}
+  currentTask = {};
 }
 
-openTaskFormBtn.addEventListener("click", () => {
-  taskForm.classList.toggle("hidden");
-})
+if (taskData.length) {
+  updateTaskContainer()
+}
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
 
 closeTaskFormBtn.addEventListener("click", () => {
-   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-   const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
-   if (formInputsContainValues  && formInputValuesUpdated) {
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+  if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
   } else {
-    reset()
+    reset();
   }
-})
-
-cancelBtn.addEventListener("click", () => {
-  confirmCloseDialog.close()
 });
 
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
 discardBtn.addEventListener("click", () => {
-  confirmCloseDialog.close()
-  // taskForm.classList.toggle("hidden")
+  confirmCloseDialog.close();
+   // taskForm.classList.toggle("hidden")
   reset()
-  // reset() -> when you the Discard button is clicked, everything in the input fields should go away.
-})
+   // reset() -> when you the Discard button is clicked, everything in the input fields should go away.
+});
 
 // get the values from the input fields, save them into the taskData array, and display them on the page
 taskForm.addEventListener("submit", (e) => {
@@ -118,4 +124,4 @@ taskForm.addEventListener("submit", (e) => {
 
   addOrUpdateTask();
 });
-  // reset() -> Clear the input fields and also hide the form modal for the user to see the added task
+// reset() -> Clear the input fields and also hide the form modal for the user to see the added task
