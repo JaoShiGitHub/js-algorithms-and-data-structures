4ce2a83ddcdf4c6e169bfdb8d75af3e1acf4308a
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-08 17:57:45 +0700

    Update script.js

--- c2/u3-decimal-to-binary/script.js
+++ c2/u3-decimal-to-binary/script.js
@@ -7,10 +7,24 @@ const decimalToBinary = (input) => {
   const quotients = [];
   const remainders = [];
 
+  if (input === 0) {
+    result.innerText = "0"
+    return
+  }
+
   while (input > 0) {
-    input = 0;
+    const quotient = Math.floor(input / 2);
+    const remainder  =  input % 2;
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
   };
-}
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
+};
 
 const checkUserInput = () => {
   if (
