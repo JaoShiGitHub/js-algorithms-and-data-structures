f1f843de11f33179f8cefb67664b4afddeeb83d2
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-10 23:47:53 +0700

    Update showAnimation()

--- c2/u3-decimal-to-binary/script.js
+++ c2/u3-decimal-to-binary/script.js
@@ -50,8 +50,10 @@ const showAnimation = () => {
     }, obj.addElDelay);
 
     setTimeout(() => {
-
+      document.getElementById(obj.inputVal).textContent = obj.msg;
     }, obj.showMsgDelay);
+
+setTimeout(() => {}, obj.removeElDelay);
   });
 };
 
