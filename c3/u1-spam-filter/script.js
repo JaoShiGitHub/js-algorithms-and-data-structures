a7d00950578beec4505fbdbe0570f8b88b630d91
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-12 07:21:43 +0700

    regex

--- c3/u1-spam-filter/script.js
+++ c3/u1-spam-filter/script.js
@@ -2,7 +2,13 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const freeRegex = /\sfr[e3][e3] m[o0]n[e3]y\s/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
@@ -10,5 +16,8 @@ checkMessageButton.addEventListener("click", () => {
     return;
   }
 
-
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
 });
