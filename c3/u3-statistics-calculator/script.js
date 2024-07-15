952f428a9929ad7618e931df5a2ad02e74c21231
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-15 15:46:52 +0700

    Cal & display mean

--- c3/u3-statistics-calculator/script.js
+++ c3/u3-statistics-calculator/script.js
@@ -1,3 +1,18 @@
+// clean up the logic
+const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+// const getMean = (array) => {
+//   const sum = array.reduce((acc, el) => acc + el, 0);
+//   const mean = sum / array.length;
+//   return mean;
+// }
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  // use method chaining instead of doing the line 15
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  // const filtered = numbers.filter(el => !isNaN(el));
+  const mean = getMean(numbers);
+  document.querySelector("#mean").textContent = mean;
 }
