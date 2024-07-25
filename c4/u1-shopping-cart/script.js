003073ca722fe3411a19414b7d7a49293cc0b595
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-25 16:14:44 +0700

    calculate tax / SubTotal &  add event

--- c4/u1-shopping-cart/script.js
+++ c4/u1-shopping-cart/script.js
@@ -132,6 +132,21 @@ class ShoppingCart {
       </div>
       `;
   }
+
+  getCounts() {
+    return this.items.length;
+  }
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+  }
 };
 
 const cart = new ShoppingCart();
@@ -141,7 +156,13 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
   (btn) => {
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
     })
   }
 );
 
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+  showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+  cartContainer.style.display = isCartShowing ? "block" : "none";
+});
