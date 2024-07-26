9d24f4473acb29759cb81d2d7045c0a0c3d0e997
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-26 19:55:07 +0700

    Create script.js & Player class

---
+++ c4/u2-platformer-game/script.js
@@ -0,0 +1,37 @@
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = (size) => {
+  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+}
+
+class Player {
+  constructor() {
+    this.position = {
+      x: proportionalSize(10),
+      y: proportionalSize(400),
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(40);
+  }
+  
+// For creating the player's width, height, position, and fill color
+draw() {
+  ctx.fillStyle = "#99c9ff";  // player colour
+  ctx.fillRect(this.position.x, this.position.y, this.width, this.height); // player's shape
+ }
+
+}
