b05a8022fa336b1b8d10a185198172f14f027d84
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-07-30 18:30:42 +0700

    Dice Game Setup 2

--- c4/u3-dice-game/script.js
+++ c4/u3-dice-game/script.js
@@ -1,18 +1,19 @@
-const listOfAllDice = document.querySelector("#dice > .die");
-const scoreInputs = document.querySelector("#score-options > div > input");
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
 const currentRound = document.getElementById("current-round");
 const currentRoundRolls = document.getElementById("current-round-rolls");
 const totalScore = document.getElementById("total-score");
 const scoreHistory = document.getElementById("score-history");
+
 const rollDiceBtn = document.getElementById("roll-dice-btn");
 const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
 const rulesBtn = document.getElementById("rules-btn");
-const rulesContainer = document.getElementsByClassName("rules-container");
 
-const isModalShowing = rulesBtn.addEventListener("click", () => rulesContainer.toggle("hidden"));
-const diceValuesArr;
-
-let rolls = 0;
+let diceValuesArr = [];
+let isModalShowing = false;
 let score = 0;
 let total = 0;
-let round = 1;
+let round = 1; 
+let rolls = 0; 
