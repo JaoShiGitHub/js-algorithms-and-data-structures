6c8d8841701712fa3b80ec7cd2e76852a475b722
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-07 17:46:13 +0700

    Populating data inside postsContainer 2: Time Activity
    • Get time to show time activity
    • Convert time to a more readable format

--- c5/u2-fcc-forum-leaderboard/script.js
+++ c5/u2-fcc-forum-leaderboard/script.js
@@ -5,10 +5,29 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
-const timeAgo = time => {
+const timeAgo = (time) => {
   const currentTime = new Date(); //  the current date and time;
   const lastPost = new Dates(time); // the date of the last activity on a topic
+
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
+
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
+  }
+
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
+  }
+
+  return `${daysAgo}d ago`;
 };
+
+const viewCount = views => views >= 1000 ? `${Math.floor(views / 1000)}k` : views;
 
 const fetchData = async () => {
   try {
@@ -33,7 +52,8 @@ const showLatestPosts = (data) => {
       views,
       posts_count,
       slug,
-      posters,_id,
+      posters,
+      category_id,
       bumped_at,
     } = item;
 
@@ -45,7 +65,7 @@ const showLatestPosts = (data) => {
       <td></td>
       <td>${posts_count - 1}</td>
       <td>${views}</td>
-      <td></td>
+      <td>${timeAgo(bumped_at)}</td>
     </tr>`;
   }).join("");
 };
