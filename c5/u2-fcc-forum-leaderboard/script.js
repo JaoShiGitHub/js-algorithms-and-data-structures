b2140218d04162ec99b0f09f52e1e832a8968f36
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-07 19:28:13 +0700

    Display the user avatars

--- c5/u2-fcc-forum-leaderboard/script.js
+++ c5/u2-fcc-forum-leaderboard/script.js
@@ -79,7 +79,7 @@ const avatars = (posters, users) => {
         : avatar;
       return `<img src="${userAvatarUrl}" alt="${user.name}"/>`
     }
-  });
+  }).join("");
 };
 
 const fetchData = async () => {
@@ -112,10 +112,12 @@ const showLatestPosts = (data) => {
     return `
     <tr>
       <td>
-        <p class="post-title">${title}</p>
+        <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
         ${forumCategory(category_id)}
       </td>
-      <td></td>
+      <td>
+      <div class="avatar-container">${avatars(posters, users)}</div>
+      </td>
       <td>${posts_count - 1}</td>
       <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
