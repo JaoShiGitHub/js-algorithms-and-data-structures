7262f62f8fc00fd6b3eac988becef8d3fb6ac97f
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-07 17:18:14 +0700

    Populating data inside postsContainer 1

--- c5/u2-fcc-forum-leaderboard/script.js
+++ c5/u2-fcc-forum-leaderboard/script.js
@@ -5,4 +5,47 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
-const fetchData = async () => {};
+const timeAgo = time => {
+  const currentTime = new Date(); //  the current date and time;
+  const lastPost = new Dates(time); // the date of the last activity on a topic
+};
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
+
+  postsContainer.innerHTML = topics.map((item) => {
+    const {
+      id,
+      title,
+      views,
+      posts_count,
+      slug,
+      posters,_id,
+      bumped_at,
+    } = item;
+
+    return `
+    <tr>
+      <td>
+        <p class="post-title">${title}</p>
+      </td>
+      <td></td>
+      <td>${posts_count - 1}</td>
+      <td>${views}</td>
+      <td></td>
+    </tr>`;
+  }).join("");
+};
