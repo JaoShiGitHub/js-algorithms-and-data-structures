21b49609a8a963c326a7c322731cf18ff35bd7ca
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-08 23:18:48 +0700

    number conditions

--- certification-projects/c2-roman-numeral-converter/script.js
+++ certification-projects/c2-roman-numeral-converter/script.js
@@ -2,14 +2,19 @@ const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 const number = document.getElementById("number")
 
-
 const convertNumber = () => {
   const input = number.value;
+  const numberInput = parseInt(input);
 
-  if (input === "") {
-    alert("Please enter a valid number")
+  if (!input) {
+    output.textContent = "Please enter a valid number";
   }
 
+  if (numberInput <= 0) {
+    output.textContent = "Please enter a number greater than or equal to 1";
+  } else if (numberInput <= 4000) {
+    output.textContent = "Please enter a number less than or equal to 3999";
+  }
 }
 
 convertBtn.addEventListener("click", convertNumber)
