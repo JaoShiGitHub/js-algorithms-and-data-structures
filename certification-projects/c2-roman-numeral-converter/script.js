ea203290767ccd7b2f16d436d80edcfe680eedf1
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-08 23:59:20 +0700

    Create romanNumerals & numbersConverted

--- certification-projects/c2-roman-numeral-converter/script.js
+++ certification-projects/c2-roman-numeral-converter/script.js
@@ -1,10 +1,27 @@
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
-const number = document.getElementById("number")
+const number = document.getElementById("number");
+
+let romanNumerals = {
+  1: "M",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
 
 const convertNumber = () => {
   const input = number.value;
   const numberInput = parseInt(input);
+  let numbersConverted = [];
 
   if (!input) {
     output.textContent = "Please enter a valid number";
@@ -12,12 +29,12 @@ const convertNumber = () => {
 
   if (numberInput <= 0) {
     output.textContent = "Please enter a number greater than or equal to 1";
-  } else if (numberInput <= 4000) {
+  } else if (numberInput <= 4000 || numberInput > 4000) {
     output.textContent = "Please enter a number less than or equal to 3999";
   }
-}
+};
 
-convertBtn.addEventListener("click", convertNumber)
+convertBtn.addEventListener("click", convertNumber);
 
 convertBtn.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
