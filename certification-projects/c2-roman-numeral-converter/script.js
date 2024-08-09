8e778658ff514b6643de0421bbdcbbe8a4d9581f
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-09 17:32:47 +0700

    Convert 2 Idx (wait to fix)

--- certification-projects/c2-roman-numeral-converter/script.js
+++ certification-projects/c2-roman-numeral-converter/script.js
@@ -30,13 +30,46 @@ const oneNumber = (num) => {
   } else if (num === 5) {
     numbersConverted.push(romanNumerals["5"]);
   } else if (num > 5 && num < 9) {
-      numbersConverted.push("V");
-      for (let i = 0; i < num - 5; i++) {
+    numbersConverted.push("V");
+    for (let i = 0; i < num - 5; i++) {
       numbersConverted.push("I");
     }
   } else if (num === 9) {
-      numbersConverted.push(romanNumerals["9"]);
+    numbersConverted.push(romanNumerals["9"]);
   }
+};
+
+// else if (firstIdx > 4 && firstIdx < 5) {
+//   numbersConverted.push("L");
+//   for (let i = 0; i < firstIdx - 5; i++) {
+
+//   }
+// }
+
+const twoLength = (numArr, num) => {
+  const firstIdx = numArr[0];
+  const secondIdx = numArr[1];
+
+  if (num === 10) {
+    numbersConverted.push(romanNumerals["10"]);
+  } else if (firstIdx < 4) {
+    for (let i = 0; i < firstIdx; i++) {
+      numbersConverted.push("X");
+    }
+  } else if (num === 40) {
+    numbersConverted.push(romanNumerals["40"]);
+  } else if (firstIdx === 4) {
+    numbersConverted.push("XL");
+  } else if (num === 50) {
+    numbersConverted.push(romanNumerals["50"]);
+  } else if (firstIdx > 5 && firstIdx < 9) {
+    numbersConverted.push("L");
+    for (let i = 0; i < firstIdx - 5; i++) {
+      numbersConverted.push("X");
+    }
+  }
+
+  oneNumber(secondIdx);
 };
 
 const convertNumber = () => {
@@ -54,6 +87,11 @@ const convertNumber = () => {
   } else {
     if (splitNumsLength === 1) {
       oneNumber(numberInput);
+    } else if (splitNumsLength === 2) {
+      twoLength(
+        splitNums.map((num) => parseInt(num)),
+        numberInput
+      );
     }
   }
   output.textContent = numbersConverted.join("");
