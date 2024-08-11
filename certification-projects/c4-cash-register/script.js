529b63131d56f765b00e958070cc81aa17b56d4b
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-11 18:56:42 +0700

    Check cash 1
    Chack if cash lessthan or equal to the price

--- certification-projects/c4-cash-register/script.js
+++ certification-projects/c4-cash-register/script.js
@@ -1,12 +1,28 @@
+const cashInputValue = document.getElementById("cash");
+const changeDue = document.getElementById("change-due");
+const purchaseBtn = document.getElementById("purchase-btn");
+
 let price = 1.87;
 let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
+
+const handleCheckCash = () => {
+  const cash = Number(cashInputValue.value);
+
+  if (cash < price) {
+    alert("Customer does not have enough money to purchase the item");
+  } else if (cash === price) {
+    alert("No change due - customer paid with exact cash");
+  }
+};
+
+purchaseBtn.addEventListener("click", handleCheckCash);
