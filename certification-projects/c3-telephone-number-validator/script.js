8d8544c25c0c1e27ba871942460e8c669012ca43
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-11 16:51:19 +0700

    handleCheckPhoneNumber conditions

--- certification-projects/c3-telephone-number-validator/script.js
+++ certification-projects/c3-telephone-number-validator/script.js
@@ -3,14 +3,30 @@ const clearBtn = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const result = document.getElementById("results-div");
 
-const input = userInput.value;
+const invalidText = "Invalid US number:";
+const validText = "Valid US number:";
+
+const handleCheckPhoneNumber = (phoneNumber) => {
+  const number = phoneNumber.replace(/[-\s]+/g, "");
+  const numLength = number.length;
+
+  if (numLength === 10 || (numLength === 11 && number.startsWith("1"))) {
+    result.textContent = `${validText} ${phoneNumber}`;
+  } else {
+    result.textContent = `${invalidText} ${phoneNumber}`;
+  }
+};
 
 const handleClickCheck = () => {
+  const input = userInput.value;
+
   if (!input) {
     alert("Please provide a phone number");
     console.log(input);
   } else {
+    userInput.value = "";
     alert("welcome to Telephone Number Validator");
+    handleCheckPhoneNumber(input);
   }
 };
 
@@ -18,5 +34,5 @@ const handleClear = () => {
   result.textContent = "";
 };
 
-checkBtn.addEventListener("click", handleClickCheck);
+checkBtn.addEventListener("click", () => handleClickCheck);
 clearBtn.addEventListener("click", handleClear);
