3acd2fc29a7dc64313e6a7296aa5e7afb2edb5e2
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-31 17:15:41 +0700

    showChange

--- certification-projects/c4-cash-register/script.js
+++ certification-projects/c4-cash-register/script.js
@@ -2,34 +2,22 @@ const cashInputValue = document.getElementById("cash");
 const displayChangeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
 const showPrice = document.getElementById("price");
+const showChange = document.getElementById("change");
 
-const price = 19.5;
-showPrice.textContent = price;
-
-// let cid = [
-//   ["PENNY", 1.01],
-//   ["NICKEL", 2.05],
-//   ["DIME", 3.1],
-//   ["QUARTER", 4.25],
-//   ["ONE", 90],
-//   ["FIVE", 55],
-//   ["TEN", 20],
-//   ["TWENTY", 60],
-//   ["ONE HUNDRED", 100],
-// ];
+const price = 3.26;
+showPrice.textContent = `Price: ${price}`;
 
 let cid = [
-  ["PENNY", 0.5],
-  ["NICKEL", 0],
-  ["DIME", 0],
-  ["QUARTER", 0],
-  ["ONE", 0],
-  ["FIVE", 0],
-  ["TEN", 0],
-  ["TWENTY", 0],
-  ["ONE HUNDRED", 0],
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
-
 const cidTotal = cid
   .reduce((acc, currentElem) => {
     const currPrice = currentElem[1];
@@ -38,11 +26,13 @@ const cidTotal = cid
   .toFixed(2);
 
 const handleCheckCash = () => {
+  displayChangeDue.innerHTML = "";
   const cash = Number(cashInputValue.value);
   let changeDue = (cash - price).toFixed(2);
   let cidStatus = { status: "OPEN", change: [] };
   let reversedCid = [...cid].reverse();
   let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  showChange.textContent = `Change: ${changeDue}`;
   cashInputValue.value = "";
 
   if (cash < price) {
@@ -55,8 +45,6 @@ const handleCheckCash = () => {
     return (displayChangeDue.textContent =
       "No change due - customer paid with exact cash");
   }
-  console.log("cidTotal: ", cidTotal);
-  console.log("changeDue: ", changeDue);
 
   if (cidTotal === changeDue || (price < cash && cidTotal === changeDue)) {
     cidStatus.status = "CLOSED";
@@ -69,7 +57,6 @@ const handleCheckCash = () => {
       while (total > 0 && changeDue >= denominations[i]) {
         total -= denominations[i];
         changeDue = parseFloat((changeDue -= denominations[i]).toFixed(2));
-        console.log("changeDue: ", changeDue, "denomination: ", reversedCid[i]);
         count++;
       }
       if (count > 0) {
@@ -81,10 +68,6 @@ const handleCheckCash = () => {
   if (changeDue > 0) {
     return (displayChangeDue.textContent = `Status: INSUFFICIENT_FUNDS`);
   }
-  console.log("length:", displayChangeDue.children.length);
-  console.log("check change:", cidStatus.change);
-  console.log("check change 0:", cidStatus.change[0]);
-  console.log("check change 1:", cidStatus.change[1]);
 
   if (cidStatus.change.length === 1) {
     displayChangeDue.innerHTML += `<p>Status: ${cidStatus.status} ${cidStatus.change[0][0]}: $${cidStatus.change[0][1]}</p>`;
@@ -94,8 +77,6 @@ const handleCheckCash = () => {
       .map((money) => `<p>${money[0]}: $${money[1]}</p>`)
       .join("");
   }
-
-  console.log("New check length:", displayChangeDue.children.length);
   return;
 };
 
